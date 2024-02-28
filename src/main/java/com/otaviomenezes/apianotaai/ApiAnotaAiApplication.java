ccbcba3f64b023c09d60ab4469413ab7045e0d68
Otavio <cmrenanotavio@gmail.com>
2024-02-28 12:10:52 -0300

    first commit

---
+++ src/main/java/com/otaviomenezes/apianotaai/ApiAnotaAiApplication.java
@@ -0,0 +1,13 @@
+package com.otaviomenezes.apianotaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAnotaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAnotaAiApplication.class, args);
+	}
+
+}
