9b61bd573fb74093a3c42d78a7f755989d51ab18
Otavio <cmrenanotavio@gmail.com>
2024-04-10 16:00:49 -0300

    Enable mongoDB

--- src/main/java/com/otaviomenezes/apianotaai/ApiAnotaAiApplication.java
+++ src/main/java/com/otaviomenezes/apianotaai/ApiAnotaAiApplication.java
@@ -2,12 +2,12 @@ package com.otaviomenezes.apianotaai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class ApiAnotaAiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiAnotaAiApplication.class, args);
 	}
-
 }
