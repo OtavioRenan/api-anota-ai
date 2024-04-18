df63df3453009d8161fb74f8e8e1a4496e7424ae
Otavio <cmrenanotavio@gmail.com>
2024-04-18 14:27:13 -0300

    refactoring products DB

--- src/main/java/com/otaviomenezes/apianotaai/infra/adapters/mongo/ProductMongoRepository.java
+++ src/main/java/com/otaviomenezes/apianotaai/infra/adapters/mongo/ProductMongoRepository.java
@@ -2,8 +2,13 @@ package com.otaviomenezes.apianotaai.infra.adapters.mongo;
 
 import com.otaviomenezes.apianotaai.infra.adapters.entities.ProductEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductMongoRepository extends MongoRepository<ProductEntity, String> {
+    @Query("{ownerId: ?0}")
+    List<ProductEntity> findByOwnerId(String ownerId);
 }
