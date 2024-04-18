dc14b479485dca9aa512e1806d01a9d2912f8d9d
Otavio <cmrenanotavio@gmail.com>
2024-04-18 14:28:13 -0300

    created product bean

--- src/main/java/com/otaviomenezes/apianotaai/infra/configs/BeanConfig.java
+++ src/main/java/com/otaviomenezes/apianotaai/infra/configs/BeanConfig.java
@@ -1,8 +1,11 @@
 package com.otaviomenezes.apianotaai.infra.configs;
 
 import com.otaviomenezes.apianotaai.domain.adapters.CategoryServiceImp;
+import com.otaviomenezes.apianotaai.domain.adapters.ProductServiceImp;
 import com.otaviomenezes.apianotaai.domain.ports.interfaces.CategoryServicePort;
+import com.otaviomenezes.apianotaai.domain.ports.interfaces.ProductServicePort;
 import com.otaviomenezes.apianotaai.domain.ports.repositories.CategoryRepositoryPort;
+import com.otaviomenezes.apianotaai.domain.ports.repositories.ProductRepositoryPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +14,10 @@ public class BeanConfig {
     @Bean
     CategoryServicePort categoryService(CategoryRepositoryPort repository) {
         return new CategoryServiceImp(repository);
+    }
+
+    @Bean
+    ProductServicePort productService(ProductRepositoryPort repository, CategoryServicePort categoryService) {
+        return new ProductServiceImp(repository, categoryService);
     }
 }
