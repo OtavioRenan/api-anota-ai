6cb67163f03f9f6804bca15c80254ebf1620196d
Otavio <cmrenanotavio@gmail.com>
2024-04-18 14:26:59 -0300

    created products ports

--- src/main/java/com/otaviomenezes/apianotaai/domain/ports/interfaces/ProductServicePort.java
+++ src/main/java/com/otaviomenezes/apianotaai/domain/ports/interfaces/ProductServicePort.java
@@ -1,4 +1,19 @@
 package com.otaviomenezes.apianotaai.domain.ports.interfaces;
 
+import com.otaviomenezes.apianotaai.domain.dtos.ProductDTO;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
+
 public interface ProductServicePort {
+    ResponseEntity<ProductDTO> delete(String id);
+
+    ResponseEntity<List<ProductDTO>> findAll(Map<String, String> inputs);
+
+    ResponseEntity<ProductDTO> findById(String id);
+
+    ResponseEntity<ProductDTO> insert(ProductDTO product);
+
+    ResponseEntity<ProductDTO> update(ProductDTO product, String id);
 }
--- src/main/java/com/otaviomenezes/apianotaai/domain/ports/repositories/ProductRepositoryPort.java
+++ src/main/java/com/otaviomenezes/apianotaai/domain/ports/repositories/ProductRepositoryPort.java
@@ -1,4 +1,17 @@
 package com.otaviomenezes.apianotaai.domain.ports.repositories;
 
+import com.otaviomenezes.apianotaai.domain.Product;
+
+import java.util.List;
+
 public interface ProductRepositoryPort {
+    void delete(Product product);
+
+    List<Product> findAll();
+
+    Product findById(String id);
+
+    List<Product> findByOwnerId(String ownerId);
+
+    Product save(Product product);
 }
